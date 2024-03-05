ef939c92a3dd207ac4593ae8a94a59766afc48ad
Daniel Minty <60455996+Dminty87@users.noreply.github.com>
2024-03-04 20:06:24 -0800

    Add project files.

---
+++ GA_BinarySearch_DanielMinty/BinarySearch.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_BinarySearch_DanielMinty
+{
+    internal static class BinarySearch
+    {
+        //public RBS method with minimal parameters to begin the recursion
+        public static int RecursiveBinarySearch(int[] array, int target)
+        {
+            return RecursiveBinarySearch(array, target, 0, array.Length - 1);
+        }
+
+        //private RBS method with all parameters to perform the recursion
+        private static int RecursiveBinarySearch(int[] array, int target, int left, int right)
+        {
+            //Base case: Left > Right
+            if (left > right)
+            {
+                return -1;
+            }
+
+            //Calculate Middle Index
+            int middleIndex = (right - left) / 2 + left;
+
+            //Compare Middle value to target value
+            //If the middle element matches the target element, return the middle Index
+            if (target == array[middleIndex])
+            {
+                return middleIndex;
+            }
+            //If the target element is less than the middle element,
+            if (target < array[middleIndex])
+            {//call RecursiveBinarySearch restricting the search to between left and middle(excluded)
+                return RecursiveBinarySearch(array, target, left, middleIndex - 1);
+            }
+            //If the target element is greater than the middle element,
+            if (target > array[middleIndex])
+            {//call RecursiveBinarySearch restricting the search to between middle(excluded) and right
+                return RecursiveBinarySearch(array, target, middleIndex + 1, right);
+            }
+
+            //Make all code paths return a value
+            return -1;
+        }
+
+        public static int IterativeBinarySearch(int[] array, int target)
+        {
+            //Left and right pointers restrict the area of the search each iteration
+            int left = 0;
+            int right = array.Length - 1;
+
+            //while the left and right pointers haven't passed eachother, there is still more array to search
+            while (left <= right)
+            {
+                //calculate the index in the middle of the l/r pointers
+                int middleIndex = (right - left) / 2 + left;
+
+                //Compare the target to the middle value
+                //if the middle value matches the target element, return the middle index
+                if (target == array[middleIndex])
+                {
+                    return middleIndex;
+                }
+                //if the target is less than the middle value, exclude the right half of the array from the search
+                if (target < array[middleIndex])
+                {
+                    right = middleIndex - 1;
+                }
+                else//the target is greater than the middle value, exclude the left half of the array from the search
+                {
+                    left = middleIndex + 1;
+                }
+            }//when the left and right pointers pass eachother,
+            //the array has been searched and the target was not found
+            return -1;
+
+        }// method IterativeBinarySearch
+
+    }//class BinarySearch
+}
---
+++ GA_BinarySearch_DanielMinty/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GA_BinarySearch_DanielMinty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maximum and minimum values for randomly generating the size of the array
+            //Program will crash if the array has length 0
+            int arraySizeMin = 10;
+            int arraySizeMax = 10;
+
+            //Maximum and minimum values for randomly generating the values stored in the array and the target value of the binary search
+            int arrayValueMin = 0;
+            int arrayValueMax = 10;
+
+            //Determines whether the target element should be guaranteed to exist in the array
+            bool guaranteeTarget = false;
+
+            //Timers for the recursive and iterative searches
+            Stopwatch recursiveTimer = new Stopwatch();
+            Stopwatch iterativeTimer = new Stopwatch();
+
+            //Create an instance of the Random class
+            Random rand = new Random();
+
+            //Create an array with a random size restricted by arraySizeMin/Max
+            int[] array = new int[rand.Next(arraySizeMin, arraySizeMax)];
+
+            //Populate the array with random values restricted by arrayValueMin/Max
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(arrayValueMin, arrayValueMax);
+            }
+
+            //Randomize the target value for the binary search with the same restrictions as the array values
+            int target = rand.Next(arrayValueMin, arrayValueMax);
+
+            //If the target shold be guaranteed in the array, override it into a random position
+            if (guaranteeTarget)
+            {
+                array[rand.Next(0, array.Length - 1)] = target;
+            }
+
+            //Sort the array
+            Array.Sort(array);
+
+            //Display the sorted array
+            Console.Write("Sorted Array: ");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+            Console.WriteLine(array[array.Length - 1] + ".");
+
+            //Display the target value
+            Console.WriteLine($"Target Value: {target}");
+
+            //Time the recursive binary search
+            recursiveTimer.Start();
+            int targetIndexR = BinarySearch.RecursiveBinarySearch(array, target);
+            recursiveTimer.Stop();
+
+            //Time the iterative binary search
+            iterativeTimer.Start();
+            int targetIndexI = BinarySearch.IterativeBinarySearch(array, target);
+            iterativeTimer.Stop();
+
+            //Display the result of the recursive binary search
+            if (targetIndexR < 0)
+            {
+                Console.WriteLine($"Recursive: The target value was not found in {recursiveTimer.ElapsedMilliseconds} miliseconds.");
+            }
+            else
+            {
+                Console.WriteLine($"Recursive: The target value was found at index {targetIndexR} in {recursiveTimer.ElapsedMilliseconds} miliseconds.");
+            }
+
+            //Display the result of the iterative binary search
+            if (targetIndexR < 0)
+            {
+                Console.WriteLine($"Iterative: The target value was not found in {iterativeTimer.ElapsedMilliseconds} miliseconds.");
+            }
+            else
+            {
+                Console.WriteLine($"Iterative: The target value was found at index {targetIndexI} in {iterativeTimer.ElapsedMilliseconds} miliseconds.");
+            }
+
+            //Keep the window open
+            Console.ReadLine();
+        }
+    }
+}
